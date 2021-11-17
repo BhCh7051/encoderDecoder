5d7621444a81ec0f21eb2440843425a8e6cd1d1a
Bharat Chandwani <bharatchandwani1@gmail.com>
2021-11-17 18:14:51 +0530

    Implemented AffineCipher fixed decrypt

--- src/algorithms/AffineCipher/encrypt.js
+++ src/algorithms/AffineCipher/encrypt.js
@@ -2,15 +2,21 @@ String.prototype.Affineencrypt = function (a, b) {
   const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
   let array = [];
   for (let i of this) {
-    array.push(alphabet.indexOf(i.toLowerCase()));
+    if (i !== ' ')
+      array.push(alphabet.indexOf(i.toLowerCase()));
+    else array.push(' ')
   }
   let output = "";
   let cle = [];
   let divtem = "";
+  console.log(array);
   for (let i of array) {
-    const image = alphabet[(i * a + b) % 26];
-    output += image;
+    if (i !== ' ') {
+      const image = alphabet[(i * a + b) % 26];
+      output += image;
+    } else output += ' ';
     const div = Math.floor((i * a + b) / 26).toString();
+
     cle.push(div);
   }
   return output;
